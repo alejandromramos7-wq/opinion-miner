f674c9021ab36f4ae1b882444259981581ee0f38
2023030546 <2023030546@upsin.edu.mx>
2026-07-16 10:02:00 -0700

    feat: agrego la limpieza y normalizacion con spacy

--- analizador.py
+++ analizador.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+import spacy
+
+try:
+    nlp = spacy.load("es_core_news_sm")
+except OSError:
+    print("Error ---> no se encontro el modelo spacy.")
+
 #agrupacion cluesterin 
 #clasificacion preciccion anova
 
@@ -20,14 +27,42 @@ def carga_base_de_datos(ruta_archivo):
     return df
 
 
+def limpiar_texto(texto):
+    #metemos el resultaod a la variable doc despues de haber hecho todo el texto en minusculas y analizado por spacy
+    doc = nlp(str(texto).lower())
+
+    #agregamos una lista vacia donde caeran las palabras limpias por un bucle
+    palabras_limpias = []
+
+    for token in doc:
+        #is_stop --> todas las palabras que que no sean paradas como el, la , los, un , de , para, que (no aportan nada al analisis)
+        #is_puntc --> todas las que no sean signos de puntuacion(elimina puntos, comas, signos de interrogacion. etc)
+        #is_espace -->que no sean espacio blanco en blanco extra un tabulador y salto de linea
+        #todos los mencionados con el not es que no los quiero 
+        if not token.is_stop and not token.is_punct and not token.is_space:
+            #lemma_ --> reduccion a la raiz, leatiza las palabras transformandolas a su infinitivo o cambian a su masculino singular
+            palabras_limpias.append(token.lemma_)
+
+    #agrega un epacio junto a cada palabra
+    return " ".join(palabras_limpias)
+
+
+
 if __name__ == "__main__":
     ARCHIVO = "opiniones_prueba.csv"
 
     try:
         datos = carga_base_de_datos(ARCHIVO)
         
-        print("\n vista previa de los primeros 3 registros")
-        print(datos.head(3))
+        print("\n Limpiando y normalizando datos...")
+        datos['comentario_limpio'] = datos['comentario'].apply(limpiar_texto)
+        print("Limpieza completa")
+
+        #solo es una comparativa para ver si se analizaron y limpiaron correctamente
+        print("\n comparativa de textos Original vs Limpio")
+        for i, fila in datos.head(3).iterrows():
+            print(f"[Original]: {fila['comentario']}")
+            print(f"[Limpio]: {fila['comentario_limpio']}")
     
     except FileNotFoundError:
         print(f"Error: No encontre el archivo '{ARCHIVO}'.")
